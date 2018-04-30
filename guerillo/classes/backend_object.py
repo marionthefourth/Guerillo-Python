628869242236948f89a06c4a0135653ebe4e6622
Marion IV <me@github.marionthefourth.com>
2018-04-29 22:21:13 -0400

    Added BackendTypes, Other Updates
    The BackendType is meant to replace AuxiliaryType for all of the Backend Objects.
    Used to more simply determine the type of a backend object.
    - - - -
    Fixed from_dictionary method to properly handle the received data back from firebase.
    - - -
    Updated Gitignore to ignore hebrewprober.py

--- guerillo/classes/backend_object.py
+++ guerillo/classes/backend_object.py
@@ -1,7 +1,22 @@
+from enum import Enum
+
 from guerillo.backend.backend import Backend
 
 
+class BackendType(Enum):
+    USER = "User"
+    LOCK = "Lock"
+    AUX = "Auxiliary"
+    COUNTY = "County"
+    DEFAULT = "Default"
+    KEYCHAIN = "Keychain"
+    REQUEST_QUEUE = "RequestQueue"
+    HOMEOWNER_SEARCH_RESULT = "HomeownerSearchResult"
+
+
 class BackendObject:
+
+    type = BackendType.DEFAULT
 
     def __init__(self, uid=None):
         if uid is not None:
@@ -12,11 +27,20 @@ class BackendObject:
     def generate_uid(self):
         self.uid = Backend.get().database().generate_key()
 
+    @staticmethod
+    def pyre_to_dictionary(obj=None):
+        dictionary = dict()
+        for item in obj.pyres:
+            dictionary[item.item[0]] = item.item[1]
+
+        return dictionary
+
     def from_dictionary(self, obj=None):
-        self.uid = obj["uid"]
+        dictionary = BackendObject.pyre_to_dictionary(obj=obj)
+        self.uid = dictionary["uid"]
+        return dictionary
 
     def to_dictionary(self):
         return {
             "uid": self.uid
         }
-
