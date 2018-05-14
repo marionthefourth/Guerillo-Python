3b09c586e565c4eccc95762ee6a3c4c58a34129d
Marion IV <me@github.marionthefourth.com>
2018-05-14 01:55:28 -0400

    Removed Action functionlity from Driver Utils, Other Small Changes
    - Removed all Action functionality from Driver Utils
    - Moved Driver Utils to a driver_utils folder
    
    - Fixed bug where Pinellas Scraper cannot compare text due to an excessive character.
    - Homeowner Search Result now returns the number of removed items when it cleans its list

--- guerillo/classes/scrapers/scraper.py
+++ guerillo/classes/scrapers/scraper.py
@@ -1,10 +1,10 @@
-from guerillo.utils.driver_utils import DriverUtils
 from guerillo.classes.backend_objects.homeowner_search_result import HomeownerSearchResult
 
 
 class Scraper:
 
     def __init__(self, search_query=None, exports_path=None):
+        from guerillo.utils.driver_utils.driver_utils import DriverUtils
         self.driver_utils = DriverUtils(exports_path=exports_path)
         self.search_result = HomeownerSearchResult(search_query)
         self.search_query = search_query
---
+++ guerillo/utils/driver_utils/driver_utils.py
@@ -0,0 +1,35 @@
+from guerillo.config import General
+from guerillo.utils.driver_utils.actions.action import ActionType
+from guerillo.utils.file_storage import FileStorage
+from selenium import webdriver
+
+
+class DriverUtils:
+    def __init__(self, driver=None, exports_path=None):
+        if not driver:
+            self.create_driver(exports_path)
+        else:
+            self.driver = driver
+
+    def create_driver(self, exports_path):
+        chrome_options = webdriver.ChromeOptions()
+        prefs = {General.WebDriver.DEFAULT_DOWNLOAD_DIRECTORY: exports_path}
+        chrome_options.add_experimental_option(General.PREFS, prefs)
+        # chrome_options.add_argument("window-position=-10000,0")
+        self.driver = webdriver.Chrome(FileStorage.get_webdriver(), chrome_options=chrome_options)
+
+    def quit(self):
+        self.driver.quit()
+
+    def process(self, action=None, actions=None):
+        if actions:
+            for action in actions:
+                if action.type == ActionType.RETURN:
+                    return action.process(self.driver)
+                else:
+                    action.process(self.driver)
+        elif action:
+            if action.type == ActionType.RETURN or action.type == ActionType.COMPLEX:
+                return action.process(self.driver)
+            else:
+                action.process(self.driver)
